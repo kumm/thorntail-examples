00fd4a4b6d4f610a16f22d14e04805ac12f69f31
kumm <kumm@webstar.hu>
2019-02-05 22:26:49 +0100

    quick cdi check

--- vaadin/vaadin10/src/main/java/org/wildfly/swarm/examples/vaadin/VaadinUI.java
+++ vaadin/vaadin10/src/main/java/org/wildfly/swarm/examples/vaadin/VaadinUI.java
@@ -10,6 +10,10 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
 
 /**
  * @author Sven Ruppert
@@ -18,29 +22,32 @@ import com.vaadin.flow.theme.lumo.Lumo;
 @Route("")
 @Theme(value = Lumo.class, variant = Lumo.DARK)
 public class VaadinUI extends Composite<Div> {
+    @Inject
+    BeanManager bm;
 
-  public VaadinUI() {
-    getContent()
-        .add(new Button() {
-               {
-                 setText("click me!");
-                 addClickListener(event -> {
-                   final Notification notification = new Notification();
-                   notification.add(
-                       new VerticalLayout(
-                           new Label("Hello, I am a notification with components!"),
-                           new Button() {
-                             {
-                               setText("Bye!");
-                               addClickListener(e -> notification.close());
-                             }
-                           }
-                       ));
-                   notification.setPosition(Notification.Position.MIDDLE);
-                   notification.open();
-                 });
-               }
-             }
-        );
-  }
+    @PostConstruct
+    private void init() {
+        getContent()
+                .add(new Button() {
+                         {
+                             setText("click me!");
+                             addClickListener(event -> {
+                                 final Notification notification = new Notification();
+                                 notification.add(
+                                         new VerticalLayout(
+                                                 new Label(bm + ""),
+                                                 new Button() {
+                                                     {
+                                                         setText("Bye!");
+                                                         addClickListener(e -> notification.close());
+                                                     }
+                                                 }
+                                         ));
+                                 notification.setPosition(Notification.Position.MIDDLE);
+                                 notification.open();
+                             });
+                         }
+                     }
+                );
+    }
 }
